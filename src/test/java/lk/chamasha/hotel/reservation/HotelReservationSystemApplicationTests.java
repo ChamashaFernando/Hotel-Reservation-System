f755f813ed2a123c37d9f8fa024e3184f9fdc966
ChamashaFernando <chamashanethmi81@gmail.com>
2025-05-23 14:28:03 +0530

    Initial commit

---
+++ src/test/java/lk/chamasha/hotel/reservation/HotelReservationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.chamasha.hotel.reservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelReservationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
